0a67198f6d8d8bfb0cf5796b901b04734f46b501
Prasanna <paithankarprasanna@gmail.com>
2023-07-29 02:09:45 +0530

    update

--- setuo.cmd
+++
@@ -1,4 +0,0 @@
-python ./setupfile.py
-SET cwd=%CD% || echo failed to set cwd
-cd %userprofile%\Desktop || echo failed to change directory
-mklink GSTscraper %cwd%\gst_gui.exe || echo failed to create link
